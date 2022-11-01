387bfac9c0298371871035422a7ffab509cd3c02
Sunny-Pirate <luca@sunnyday.software>
2022-11-01 12:20:08 +0100

    Added typescript support and first page render

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+export default function BaseLayout({children}) {
+    return (<html>
+    <head>
+        <title>DreamLab.Solutions</title>
+    </head>
+    <body>
+    {children}
+    </body>
+    </html>)
+}
